2c9fa03fd20297b4305520733793ce4c280a6e4b
Giedrius <giedrius.medzevicius@gmail.com>
2017-12-07 17:56:22 +0200

    Add simple temporary ipv6 test

---
+++ ipv6_test.js
@@ -0,0 +1,28 @@
+var HOST = 'ip6-localhost';
+var SERVER_PORT = 4000;
+var CLIENT_PORT  = 4001;
+
+var dgram = require('dgram');
+
+var message = new Buffer('(data)');
+
+var server = dgram.createSocket('udp6');
+server.bind(SERVER_PORT);
+
+server.on('message', (msg, info) => {
+  console.log('Server got message: "' + msg + '"');
+  console.log(info)
+  server.close();
+});
+
+
+var client = dgram.createSocket('udp6');
+client.bind(CLIENT_PORT);
+
+client.send(message, 0, message.length, SERVER_PORT, HOST,
+  function(error, bytes) { 
+    if (error) throw error;
+    console.log('Message sent to host: "' + HOST + '", port: ' + SERVER_PORT);
+    client.close();
+  } 
+);
