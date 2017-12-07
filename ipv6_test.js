5ac36a7af36ce0cccfc8b550818005cad24fec80
Giedrius <giedrius.medzevicius@gmail.com>
2017-12-07 18:25:17 +0200

    print interfaces

--- ipv6_test.js
+++ ipv6_test.js
@@ -42,3 +42,24 @@ test(HOST, '2');
 test(false, '3');
 test('::1', '4');
 test('fe80::1', '5');
+
+
+var os = require('os');
+var ifaces = os.networkInterfaces();
+console.log(ifaces);
+
+Object.keys(ifaces).forEach(function (ifname) {
+  var alias = 0;
+
+  ifaces[ifname].forEach(function (iface) {
+    if (alias >= 1) {
+      // this single interface has multiple ipv4 addresses
+      console.log(ifname + ':' + alias, iface.address);
+    } else {
+      // this interface has only one ipv4 adress
+      console.log(ifname, iface.address);
+    }
+    ++alias;
+  });
+});
+
